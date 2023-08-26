aaa5b3e63b1679d5690fc60ee26d1ea613a39a8d
Sharon Chen <sharonhxchen@gmail.com>
2023-08-25 20:26:59 -0400

    rearrange the order of functions

--- s3_to_dynamoDB.py
+++ s3_to_dynamoDB.py
@@ -5,121 +5,6 @@ import io
 import re
 
 
-dynamodb_client = boto3.client('dynamodb')
-s3_client = boto3.client('s3')
-
-"""
-    Create a DynamoDB using given table name
-"""
-def create_dynamodb_table(table_name, region):
-    try:
-        dynamodb = boto3.resource('dynamodb', region_name=region)
-        
-        table = dynamodb.create_table(
-            TableName=table_name,
-            AttributeDefinitions = [
-                {
-                    'AttributeName': 'pk',
-                    'AttributeType': 'N'
-                }
-            ],
-            KeySchema=[
-                {
-                    'AttributeName': 'pk',
-                    'KeyType': 'HASH'
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 25,
-                'WriteCapacityUnits': 50
-            }
-        )
-        table.wait_until_exists()
-        print(f"Table '{table_name}' created successfully.")
-    except Exception as e:
-        print(f"Error creating table: {e}")
-
-"""
-    Read compressed data from S3 and export it to Data Frame
-    bucket_name is the S3 bucket name
-    object_name is the folder/object name of for corresponding snapshot
-"""
-def export_s3_to_dataframe(bucket_name, object_name):
-
-    print(f"Exporting S3 backup: {object_name}")
-
-    try:
-        
-        # Get the data from the S3 bucket
-        bucket = s3_client.list_objects_v2(Bucket=bucket_name)
-
-        dataSource = None
-        for obj in bucket['Contents']:
-            key = obj['Key']
-            if object_name in key and ".gz.parquet" in key:
-                #print(obj)
-                dataSource = s3_client.get_object(Bucket=bucket_name, Key=key)
-                break
-
-        buffer = io.BytesIO(dataSource ['Body'].read())
-
-        # Read the Parquet file from the buffer using pyarrow
-        parquet_table = pq.read_table(buffer)
-
-        # Convert the pyarrow table to a pandas DataFrame
-        df = parquet_table.to_pandas()
-
-        # Now you can work with the DataFrame 'df'
-        print(df.head())
-        print(len(df.index))
-        print("\n")
-        
-        print("Export to DataFrame completed successfully.")
-        return df
-        
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-    
-
-"""
-    Store data in DataFrame to DynamoDB
-"""
-def dataframe_to_dynamoDB(df, dynamodb_table_name):
-    if df is None:
-        print("No DataFrame is saved from S3.")
-        return 1
-
-    # Get the DynamoDB table
-    dynamodb = boto3.resource('dynamodb', region_name=region)
-    table = dynamodb.Table(dynamodb_table_name)
-
-    # Store data in the DynamoDB table in batch
-    with table.batch_writer() as batch:
-        for i, row in df.iterrows():
-            batch.put_item(Item=row.to_dict())
-
-    print("Export to DynamoDB completed successfully.")
-    return 0
-
-
-"""
-    Retrieve 10 items (in random order) from DynamoDB
-"""
-def retrieve_from_dynamoDB(dynamodb_table_name):
-    
-    response = dynamodb_client.describe_table(
-    TableName=dynamodb_table_name
-    )
-    print(response, "\n")
-
-
-    table = dynamodb.Table(dynamodb_table_name)
-
-    response = table.scan(Limit=10)
-    print(response["Items"], "\n")
-
-
 """
     Return backup folder names in given S3 bucket
     folder names are in ascending order, based on created time
@@ -170,8 +55,109 @@ def get_user_input_datetime(folder_names):
     for f in folder_names:
         if val in f:
             res = f
+            break
 
     return res
+
+
+dynamodb_client = boto3.client('dynamodb')
+s3_client = boto3.client('s3')
+
+
+"""
+    Read compressed data from S3 and export it to Data Frame
+    bucket_name is the S3 bucket name
+    object_name is the folder/object name of for corresponding snapshot
+"""
+def export_s3_to_dataframe(bucket_name, object_name):
+
+    print(f"Exporting S3 backup: {object_name}")
+
+    try:
+        
+        # Get the data from the S3 bucket
+        bucket = s3_client.list_objects_v2(Bucket=bucket_name)
+
+        dataSource = None
+        for obj in bucket['Contents']:
+            key = obj['Key']
+            if object_name in key and ".gz.parquet" in key:
+                #print(obj)
+                dataSource = s3_client.get_object(Bucket=bucket_name, Key=key)
+                break
+
+        buffer = io.BytesIO(dataSource ['Body'].read())
+
+        # Read the Parquet file from the buffer using pyarrow
+        parquet_table = pq.read_table(buffer)
+
+        # Convert the pyarrow table to a pandas DataFrame
+        df = parquet_table.to_pandas()
+
+        # Now you can work with the DataFrame 'df'
+        print(df.head())
+        print(len(df.index))
+        print("\n")
+        
+        print("Export to DataFrame completed successfully.")
+        return df
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+"""
+    Create a DynamoDB using given table name
+"""
+def create_dynamodb_table(table_name, region):
+    try:
+        dynamodb = boto3.resource('dynamodb', region_name=region)
+        
+        table = dynamodb.create_table(
+            TableName=table_name,
+            AttributeDefinitions = [
+                {
+                    'AttributeName': 'pk',
+                    'AttributeType': 'N'
+                }
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'pk',
+                    'KeyType': 'HASH'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 25,
+                'WriteCapacityUnits': 50
+            }
+        )
+        table.wait_until_exists()
+        print(f"Table '{table_name}' created successfully.")
+    except Exception as e:
+        print(f"Error creating table: {e}")
+
+    
+"""
+    Store data in DataFrame to DynamoDB
+"""
+def dataframe_to_dynamoDB(df, dynamodb_table_name):
+    if df is None:
+        print("No DataFrame is saved from S3.")
+        return 1
+
+    # Get the DynamoDB table
+    dynamodb = boto3.resource('dynamodb', region_name=region)
+    table = dynamodb.Table(dynamodb_table_name)
+
+    # Store data in the DynamoDB table in batch
+    with table.batch_writer() as batch:
+        for i, row in df.iterrows():
+            batch.put_item(Item=row.to_dict())
+
+    print("Export to DynamoDB completed successfully.")
+    return 0
 
 
 if __name__ == "__main__":
@@ -195,6 +181,6 @@ if __name__ == "__main__":
     df = export_s3_to_dataframe(s3_bucket_name, object_name)
     create_dynamodb_table(dynamodb_table_name, region)
     dataframe_to_dynamoDB(df, dynamodb_table_name)
-    #retrieve_from_dynamoDB(dynamodb_table_name, region)
+
 
 
